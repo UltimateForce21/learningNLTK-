5e1dd2931b2e764ab7848a827f586375466bdcc7
Abdullah Amin <67847674+UltimateForce21@users.noreply.github.com>
2023-12-27 21:59:22 -0500

    Changed file names

--- eps1/my_tokenize.py
+++ eps1/my_tokenize.py
@@ -1,5 +1,8 @@
 from nltk.tokenize import sent_tokenize, word_tokenize 
 
+#sent_tokenize splits tstring of text by sentences and questions
+#word_tokenize spilts string by words and punctuation
+
 example_text = "Hello Mr. Smith, how are you? The weather is great and Python is awasome. The sky is blue and python is awesome."
 
 print(sent_tokenize(example_text))
